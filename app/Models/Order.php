578b94bc9e6891fa1a9d5f6dea330cd8c404a74b
Dwain <dwaincontact@gmail.com>
2026-01-12 15:40:28 +0100

    exo 2

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Order extends Model
 {
-    protected $fillable = ['user_id', 'amount', 'status'];
+    protected $fillable = ['amount', 'status'];
 
     public function user(): BelongsTo
     {
